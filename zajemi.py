cec9c1c6745eb15732d221220f111c006e18821d
Masa Zaucer <masa.zaucer@student.fmf.uni-lj.si>
2021-10-21 23:02:17 +0200

    dodano zajemanje vec strani za vsako regijo

--- zajemi.py
+++ zajemi.py
@@ -1,7 +1,6 @@
 import requests
 
-STEVILO_STRANI = 2
-STEVILO_OGLASOV_NA_STRAN = 30
+STEVILO_STRANI = 3
 REGIJE = ['severna-primorska', 'gorenjska', 'juzna-primorska', 'notranjska', 'ljubljana-okolica', 'ljubljana-mesto', 'dolenjska', 'posavska', 'zasavska', 'savinjska', 'koroška', 'podravska', 'pomurska']
 
 
@@ -13,7 +12,8 @@ def nalozi_stran(url):
 
 
 for regija in REGIJE:
-    url = f'https://www.nepremicnine.net/oglasi-prodaja/{regija}/stanovanje/'
-    vsebina = nalozi_stran(url)
-    with open(f'nepremicnine-{regija}.html', 'w', encoding='utf-8') as f:
-        f.write(vsebina)
+    for stran in range(1, STEVILO_STRANI + 1):
+        url = f'https://www.nepremicnine.net/oglasi-prodaja/{regija}/stanovanje/{stran}/'
+        vsebina = nalozi_stran(url)
+        with open(f'podatki_nepremicnine/nepremicnine-{regija}-{stran}.html', 'w', encoding='utf-8') as f:
+            f.write(vsebina)
