36bc71f57a0aae42a473f2a08a95160fb1dffe7e
Masa Zaucer <masa.zaucer@student.fmf.uni-lj.si>
2021-10-30 00:25:01 +0200

    dodana funkcija poberi stran za klicanje v nepremicnine.py

--- zajemi.py
+++ zajemi.py
@@ -10,10 +10,10 @@ def nalozi_stran(url):
     odziv = requests.get(url, headers=headers)
     return odziv.text
 
-
-for regija in REGIJE:
-    for stran in range(1, STEVILO_STRANI + 1):
-        url = f'https://www.nepremicnine.net/oglasi-prodaja/{regija}/stanovanje/{stran}/'
-        vsebina = nalozi_stran(url)
-        with open(f'podatki_nepremicnine/nepremicnine-{regija}-{stran}.html', 'w', encoding='utf-8') as f:
-            f.write(vsebina)
+def poberi_strani():
+    for regija in REGIJE:
+        for stran in range(1, STEVILO_STRANI + 1):
+            url = f'https://www.nepremicnine.net/oglasi-prodaja/{regija}/stanovanje/{stran}/'
+            vsebina = nalozi_stran(url)
+            with open(f'podatki_nepremicnine/nepremicnine-{regija}-{stran}.html', 'w', encoding='utf-8') as f:
+                f.write(vsebina)
